95f8032b5a2dba21f6fa613a60d09f4c9519230e
Yusuf Aniki <anikiyusuf99@gmail.com>
2024-06-06 17:34:34 +0100

    fix: user controller

--- controller/userController.js
+++ controller/userController.js
@@ -104,7 +104,7 @@ const forgetPassword = async (req, res) => {
 
         await user.save({ validateModifiedOnly: true });
 
-        const resetUrl = `https://livesoccertipsss-qnq2.onrender.com/auth/resetpassword/${resetToken}`;
+        const resetUrl = `https://livesoccertipsss-qnq2.onrender.com/auth/resetpassword?token=${resetToken}&id=${user.id}`;
         const mailOptions = {
             to: user.email,
             from: 'livesoccertip@gmail.com',
